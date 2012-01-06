88a30210f32add54af498bf6f127b2b9773bdf8e
Petr Machata <pmachata@redhat.com>
2012-01-06 20:44:24 +0100

    Drop unused MAX_ARGS

--- defs.h
+++ defs.h
@@ -3,10 +3,6 @@
 #define DEFAULT_ALIGN 50	/* default alignment column for results */
 #endif				/* (-a switch) */
 
-#ifndef MAX_ARGS
-#define MAX_ARGS        32	/* maximum number of args for a function */
-#endif
-
 #ifndef DEFAULT_STRLEN
 #define DEFAULT_STRLEN  32	/* default maximum # of bytes printed in */
 #endif				/* strings (-s switch) */
