b89c2568efa32a6b0a8bba09a7abe1bfe60dd73a
Petr Machata <pmachata@redhat.com>
2012-04-03 17:00:28 +0200

    Update current column when showing caller

--- output.c
+++ output.c
@@ -175,7 +175,8 @@ output_left(enum tof type, struct Process *proc,
 	current_depth = proc->callstack_depth;
 	begin_of_line(type, proc);
 	if (!options.hide_caller)
-		fprintf(options.output, "%s->", libsym->lib->soname);
+		current_column += fprintf(options.output, "%s->",
+					  libsym->lib->soname);
 #ifdef USE_DEMANGLE
 	current_column +=
 		fprintf(options.output, "%s(",
