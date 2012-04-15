051dffaf54d2e049487bc884f3fa4346444be865
Petr Machata <pmachata@redhat.com>
2012-04-15 04:19:17 +0200

    Update commentary at insert_breakpoint

--- breakpoint.h
+++ breakpoint.h
@@ -112,12 +112,14 @@ int breakpoint_turn_on(struct breakpoint *bp, struct Process *proc);
  * on failure.  */
 int breakpoint_turn_off(struct breakpoint *bp, struct Process *proc);
 
-/* This is actually several functions rolled in one:
- *  - malloc
- *  - breakpoint_init
- *  - proc_add_breakpoint
- *  - breakpoint_enable
- * XXX I think it should be broken up somehow.  */
+/* Utility function that does what typically needs to be done when a
+ * breakpoint is to be inserted.  It checks whether there is another
+ * breakpoint in PROC for given ADDR.  If not, it allocates memory for
+ * a new breakpoint on the heap, initializes it, and calls
+ * PROC_ADD_BREAKPOINT to add the newly-created breakpoint.  For newly
+ * added as well as preexisting breakpoints, it then calls
+ * BREAKPOINT_TURN_ON.  If anything fails, it cleans up and returns
+ * NULL.  Otherwise it returns the breakpoint for ADDR.  */
 struct breakpoint *insert_breakpoint(struct Process *proc, void *addr,
 				     struct library_symbol *libsym);
 
