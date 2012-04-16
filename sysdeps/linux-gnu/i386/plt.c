1a53e0ab6ee4e53dfa6e81239deeaefdcd639082
Petr Machata <pmachata@redhat.com>
2012-04-17 00:40:25 +0200

    Fix compilation of linux-gnu/i386/plt.c

--- sysdeps/linux-gnu/i386/plt.c
+++ sysdeps/linux-gnu/i386/plt.c
@@ -1,13 +1,16 @@
 #include <gelf.h>
 #include "proc.h"
-#include "common.h"
+#include "library.h"
+#include "ltrace-elf.h"
 
 GElf_Addr
-arch_plt_sym_val(struct ltelf *lte, size_t ndx, GElf_Rela * rela) {
+arch_plt_sym_val(struct ltelf *lte, size_t ndx, GElf_Rela *rela)
+{
 	return lte->plt_addr + (ndx + 1) * 16;
 }
 
 void *
-sym2addr(Process *proc, struct library_symbol *sym) {
+sym2addr(struct Process *proc, struct library_symbol *sym)
+{
 	return sym->enter_addr;
 }
