a11cbede4711e1efe2d6fb0a8a5b2050b2fe941c
Petr Machata <pmachata@redhat.com>
2012-05-05 00:07:38 +0200

    Add include guards and copyright notices to dict.c and dict.h

--- dict.c
+++ dict.c
@@ -1,3 +1,25 @@
+/*
+ * This file is part of ltrace.
+ * Copyright (C) 2011,2012 Petr Machata
+ * Copyright (C) 2003,2004,2008,2009 Juan Cespedes
+ * Copyright (C) 2006 Ian Wienand
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,8 +28,8 @@
 #include "common.h"
 
 /*
-  Dictionary based on code by Morten Eriksen <mortene@sim.no>.
-*/
+ * Dictionary based on code by Morten Eriksen <mortene@sim.no>.
+ */
 
 struct dict_entry {
 	unsigned int hash;
--- dict.h
+++ dict.h
@@ -1,6 +1,32 @@
 /*
-  Dictionary based on code by Morten Eriksen <mortene@sim.no>.
-*/
+ * This file is part of ltrace.
+ * Copyright (C) 2011,2012 Petr Machata
+ * Copyright (C) 2003,2004,2008,2009 Juan Cespedes
+ * Copyright (C) 2006 Ian Wienand
+ * Copyright (C) ???? Morten Eriksen <mortene@sim.no>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+#ifndef _DICT_H_
+#define _DICT_H_
+
+/*
+ * Dictionary based on code by Morten Eriksen <mortene@sim.no>.
+ */
 
 typedef struct dict Dict;
 
@@ -25,3 +51,5 @@ extern Dict * dict_clone2(Dict * old,
 			  void * (* key_clone)(void * key, void * data),
 			  void * (* value_clone)(void * value, void * data),
 			  void * data);
+
+#endif /* _DICT_H_ */
