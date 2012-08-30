5e2cfb11c420af3352a62ea197f37f85c00e024c
Andreas Schwab <schwab@linux-m68k.org>
2012-08-31 01:21:55 +0200

    Fix arch_plt_sym_val for m68k

--- sysdeps/linux-gnu/m68k/plt.c
+++ sysdeps/linux-gnu/m68k/plt.c
@@ -1,11 +1,12 @@
 #include <gelf.h>
 #include "proc.h"
 #include "common.h"
+#include "library.h"
 
 GElf_Addr
 arch_plt_sym_val(struct ltelf *lte, size_t ndx, GElf_Rela * rela) {
 	return lte->plt_addr + (ndx + 1)
-	    * ((lte->ehdr.e_flags & EF_CPU32) ? 24 : 12);
+	    * ((lte->ehdr.e_flags & EF_CPU32) ? 24 : 20);
 }
 
 void *
