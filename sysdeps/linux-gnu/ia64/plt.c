85465a6248ad4f97f133803a19726eb9049f8394
Petr Machata <pmachata@redhat.com>
2012-11-08 00:36:33 +0100

    Coding style nits in ia64 backend

--- sysdeps/linux-gnu/ia64/plt.c
+++ sysdeps/linux-gnu/ia64/plt.c
@@ -25,29 +25,27 @@
 #include "common.h"
 #include "library.h"
 
-/* A bundle is 128 bits */
+/* A bundle is 128 bits.  */
 #define BUNDLE_SIZE 16
 
-/*
-
-  The PLT has
-
-  ] 3 bundles as a header
-
-  ] The special reserved entry
-
-  ] Following that, each PLT entry has it's initial code that the GOT entry
-    points to.  Each PLT entry has one bundle allocated.
-
-  ] Following that, each PLT entry has two bundles of actual PLT code,
-    i.e. load up the address from the GOT and jump to it.  This is the
-    point we want to insert the breakpoint, as this will be captured
-    every time we jump to the PLT entry in the code.
-
-*/
+/* The PLT has
+ *
+ * - 3 bundles as a header
+ *
+ * - The special reserved entry
+ *
+ * - Following that, each PLT entry has it's initial code that the GOT
+ *   entry points to.  Each PLT entry has one bundle allocated.
+ *
+ * - Following that, each PLT entry has two bundles of actual PLT
+ *   code, i.e. load up the address from the GOT and jump to it.  This
+ *   is the point we want to insert the breakpoint, as this will be
+ *   captured every time we jump to the PLT entry in the code.
+ */
 
 GElf_Addr
-arch_plt_sym_val(struct ltelf *lte, size_t ndx, GElf_Rela * rela) {
+arch_plt_sym_val(struct ltelf *lte, size_t ndx, GElf_Rela * rela)
+{
 	/* Find number of entires by removing header and special
 	 * entry, dividing total size by three, since each PLT entry
 	 * will have 3 bundles (1 for inital entry and two for the PLT
