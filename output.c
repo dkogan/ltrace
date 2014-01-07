dkogan/ltrace

commit af452c67bceba1326bb629d7c1a8241d54c09038

check return value of unw_get_reg and unw_get_proc_name
Before printng the current procedure name and IP verify their validity

output.c
@@ -661,9 +661,11 @@ output_right(enum tof type, struct process *proc, struct library_symbol *libsym,
 					!= sizeof(arch_addr_t))]);
 		unw_init_remote(&cursor, proc->unwind_as, proc->unwind_priv);
 		while (unwind_depth) {
-			unw_get_reg(&cursor, UNW_REG_IP, (unw_word_t *) &ip);
-			unw_get_proc_name(&cursor, fn_name, sizeof(fn_name),
-					(unw_word_t *) &function_offset);
+
+			if (unw_get_reg(&cursor, UNW_REG_IP, (unw_word_t *) &ip)) {
+				fprintf(options.output, " > stacktrace_error\n");
+				continue;
+			}
 
 			/* We are looking for the library with the base address
 			 * closest to the current ip.  */
@@ -683,8 +685,14 @@ output_right(enum tof type, struct process *proc, struct library_symbol *libsym,
 				lib = lib->next;
 			}
 
-			fprintf(options.output, " > %s(%s+0x%p) [%p]\n",
+			own_retval = unw_get_proc_name(&cursor, fn_name, sizeof(fn_name),
+						(unw_word_t *) &function_offset);
+			if ((own_retval == 0) || (own_retval == -UNW_ENOMEM))
+				fprintf(options.output, " > %s(%s+%p) [%p]\n",
 					lib_name, fn_name, function_offset, ip);
+			else
+				fprintf(options.output, " > %s(??\?) [%p]\n",
+					lib_name, ip);
 
 			if (unw_step(&cursor) <= 0)
 				break;

proc.c
@@ -29,11 +29,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(HAVE_LIBUNWIND)
-#include <libunwind.h>
-#include <libunwind-ptrace.h>
-#endif /* defined(HAVE_LIBUNWIND) */
-
 #include "backend.h"
 #include "breakpoint.h"
 #include "debug.h"

proc.h
@@ -30,6 +30,7 @@
 
 #if defined(HAVE_LIBUNWIND)
 # include <libunwind.h>
+# include <libunwind-ptrace.h>
 #endif /* defined(HAVE_LIBUNWIND) */
 
 #include "ltrace.h"

commit 5d3be3319b6997961283e5da61318bf46ac650a3

Fix infinite loop in stack unwind
This bug was introduced in af452c6

Reported-by: Petr Machata <pmachata@redhat.com>

output.c
@@ -662,9 +662,12 @@ output_right(enum tof type, struct process *proc, struct library_symbol *libsym,
 		unw_init_remote(&cursor, proc->unwind_as, proc->unwind_priv);
 		while (unwind_depth) {
 
-			if (unw_get_reg(&cursor, UNW_REG_IP, (unw_word_t *) &ip)) {
-				fprintf(options.output, " > stacktrace_error\n");
-				continue;
+			own_retval = unw_get_reg(&cursor, UNW_REG_IP,
+					(unw_word_t *) &ip);
+			if (own_retval) {
+				fprintf(options.output, " > Error: %s\n",
+						unw_strerror(own_retval));
+				goto cont;
 			}
 
 			/* We are looking for the library with the base address
@@ -694,6 +697,7 @@ output_right(enum tof type, struct process *proc, struct library_symbol *libsym,
 				fprintf(options.output, " > %s(??\?) [%p]\n",
 					lib_name, ip);
 
+		cont:
 			if (unw_step(&cursor) <= 0)
 				break;
 			unwind_depth--;