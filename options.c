5da68e9b29fce759fbd8c42130f1a231e2a0aadd
Petr Machata <pmachata@redhat.com>
2014-02-28 12:37:15 +0100

    Add --config to man, drop -X from options.c

--- options.c
+++ options.c
@@ -559,7 +559,7 @@ process_options(int argc, char **argv)
 #if defined(HAVE_UNWINDER)
 			"w:"
 #endif
-			"cfhiLrStTVba:A:D:e:F:l:n:o:p:s:u:x:X:";
+			"cfhiLrStTVba:A:D:e:F:l:n:o:p:s:u:x:";
 
 #ifdef HAVE_GETOPT_LONG
 		c = getopt_long(argc, argv, opts, long_options, &option_index);
